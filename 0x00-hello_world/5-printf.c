5ae7abd2c14b90170cb3d0cae5f95241e6129b4c
Martha Ndungu <martha.ndungu1389@gmail.com>
2023-03-16 13:39:46 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The main function to print proper grammer but looks ke a piece of art
+ *
+ * Return ; 0 when succefull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
