d2a7d6960b54680a05b25dc9e0c029b0b42bcbde
Martha Ndungu <martha.ndungu1389@gmail.com>
2023-03-19 09:02:50 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  prints all single digit numbers
+ *
+ * Return: Always (Successfil)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("i%d", i);
+	printf("\n");
+	return (0);
+
+
+
+}
