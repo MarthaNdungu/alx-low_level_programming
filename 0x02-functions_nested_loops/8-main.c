d3d2e3804ee16d2586e78a825154e01e270716f3
Martha Ndungu <martha.ndungu1389@gmail.com>
2023-03-22 10:58:28 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -3,11 +3,11 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  *
- * Return: Always 0.
+ * Return: Always 0.i
  */
 int main(void)
 {
 
-  void jack_bauer(void)
+  void jack_bauer('void')
     return (0);
 }
