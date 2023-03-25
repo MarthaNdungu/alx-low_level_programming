ec4ed8b0e0f185779297d224cbc8f679e849d0d1
Martha Ndungu <martha.ndungu1389@gmail.com>
2023-03-25 02:34:49 -0700

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,11 @@
 
 /**
  * __isdigit - check if numbers are 0-9
+ * Description: The program checks for digts
  * @c: char to be checked
  *
  * Return: 0 or 1
  */
-
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
